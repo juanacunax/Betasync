3690c41172c9c5fb5ccdafd398832d0b4f6ea0e7
juanacunax <juan.acunasspp@gmail.com>
2026-04-23 00:46:44 -0400

    Create GoogleAuthService.js for Google OAuth authentication

---
+++ src/modules/auth/GoogleAuthService.js
@@ -0,0 +1,24 @@
+// Service for Google OAuth authentication
+
+class GoogleAuthService {
+    constructor(clientId) {
+        this.clientId = clientId;
+    }
+
+    // Method to initialize Google Auth
+    init() {
+        // Initialization logic
+    }
+
+    // Method to sign in the user
+    signIn() {
+        // Sign-in logic
+    }
+
+    // Method to sign out the user
+    signOut() {
+        // Sign-out logic
+    }
+}
+
+export default GoogleAuthService;
